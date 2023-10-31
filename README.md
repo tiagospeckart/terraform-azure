3a27c18b1a99d974a9b539b0a3e579f10c131861
Tiago Speckart <tiagospeckart@gmail.com>
2023-10-31 13:26:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn Terraform with Azure by Building a Dev Environment – Full Course for Beginners
+
+Code implementation from [this FCC course](https://www.youtube.com/watch?v=V53AHWun17s)
---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "Brazil South"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "mtc-subnet" {
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
